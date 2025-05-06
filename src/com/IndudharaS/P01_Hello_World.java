84f5efdbc4f5e58551759168f648befbf6079304
Indudhara S <140192917+IndudharaS@users.noreply.github.com>
2025-05-06 17:44:39 +0530

    Update P01_Hello_World.java

--- src/com/IndudharaS/P01_Hello_World.java
+++ src/com/IndudharaS/P01_Hello_World.java
@@ -3,5 +3,6 @@ package com.IndudharaS;
 public class P01_Hello_World {
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
+		// Add the code to check a number is Even or Odd.
 	}
 }
