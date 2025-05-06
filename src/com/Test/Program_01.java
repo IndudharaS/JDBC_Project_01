cfcde4b8277dc1cb04ce69f8eb5d76ee0f531057
Indudhara S <indudhara2020@gmail.com>
2025-05-06 14:15:21 +0530

    Updated to print n numbers.

--- src/com/Test/Program_01.java
+++ src/com/Test/Program_01.java
@@ -5,9 +5,17 @@ public class Program_01 {
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
 		Scanner s = new Scanner(System.in);
+		
 		//Square of a number.
 		System.out.print("Enter a number: ");
 		int n = s.nextInt();
 		System.out.println("Square of "+ n +" is: "+ (n*n));
+		
+		//To Print n numbers.
+		System.out.print("Enter number to print in loop: ");
+		n = s.nextInt();
+		for (int i = 0; i < n; i++) {
+			System.out.println("The number: "+i);
+		}
 	}
 }
